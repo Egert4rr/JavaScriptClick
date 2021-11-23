0eb618dae1c69037cad4fd15f3cf94c75df45fb4
Opilane TTHK <opilane@tthk.ee>
2021-11-23 11:11:54 +0200

    first comit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const mainImage = document.querySelector('.gallery-image1 img');
+const imagesToClick = document.querySelectorAll(".gallery img");
+console.log(imagesToClick);
+
+imagesToClick.forEach(img => img.addEventListener('click', changeImage));
+
+function changeImage(event){
+
+    imagesToClick.forEach(img => (img.style.opacity=1));
+    event.target.style.opacity = 0.5 ;
+    mainImage.src = event.target.src;
+    if (event.target === mainImage) {
+        mainImage.src = "img/cat.jpg";
+        mainImage.style.opacity = 1;
+    } 
+   
+        
+    
+}
